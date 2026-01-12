8ee73dc5cffb195feac477ada0cd314e5f08181d
Yannisus <yannis.defila@mng.ch>
2026-01-12 10:58:28 +0100

    Flask_Tutorial1

--- flask_app.py
+++ flask_app.py
@@ -130,5 +130,10 @@ def complete():
     db_write("DELETE FROM todos WHERE user_id=%s AND id=%s", (current_user.id, todo_id,))
     return redirect(url_for("index"))
 
+@app.route("/users", methods=["GET"]) 
+@login_required 
+def users(): 
+pass 
+
 if __name__ == "__main__":
     app.run()
